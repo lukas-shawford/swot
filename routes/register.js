0a2f85813cd964bacc3b4fd66fd979439a0b79e0
Sergey Krilov <serg.kr@gmail.com>
2013-12-22 01:15:26 -0500

    Improved registration form, and storing emails

--- routes/register.js
+++ routes/register.js
@@ -1,28 +1,38 @@
 var User = require('../lib/user');
 
 exports.form = function (req, res) {
-    res.render('register', { title: 'Register' });
+    res.render('register', { title: 'Register', message: req.flash('error') });
 };
 
 exports.submit = function (req, res, next) {
     var data = req.body.user;
-    User.findByName(data.name, function (err, user) {
+    User.findByName(data.username, function (err, user) {
         if (err) return next(err);
 
         if (user) {
-            res.error("Username already taken!");
+            req.flash('error', 'Sorry, that username is not available.');
             res.redirect('back');
-        } else {
-            user = new User({
-                username: data.name,
-                password: data.pass
-            });
+            return;
+        }
 
-            User.createUser(user, function (err, user) {
+        if (data.password !== data.password_confirm) {
+            req.flash('error', 'The passwords you entered do not match. Please retype the passwords again.');
+            res.redirect('back');
+            return;
+        }
+
+        user = new User({
+            username: data.username,
+            email: data.email,
+            password: data.password
+        });
+
+        User.createUser(user, function (err, user) {
+            if (err) return next(err);
+            req.login(user, function (err) {
                 if (err) return next(err);
-                req.session.uid = user._id;
                 res.redirect('/');
             });
-        }
+        });
     });
 };
