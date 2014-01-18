7475e45bb53f72f3acffc06729c3b4d52ab50014
Sergey Krilov <serg.kr@gmail.com>
2014-01-18 17:28:59 -0500

    Add a 'ckedit' directive for enabling inline-editing on an element using CKEditor
    Taken from:
    http://ericpanorel.net/2013/08/03/ckeditor-4-inline-mode-angularjs-directive/

---
+++ client/app/common/ngCkEditor.js
@@ -0,0 +1,66 @@
+"use strict";
+
+angular.module('swot').directive('ckedit', function ($parse) {
+    CKEDITOR.disableAutoInline = true;
+    var counter = 0,
+    prefix = '__ckd_';
+ 
+    return {
+        restrict: 'A',
+        link: function (scope, element, attrs, controller) {
+            var getter = $parse(attrs.ckedit),
+                setter = getter.assign;
+ 
+            attrs.$set('contenteditable', true); // inline ckeditor needs this
+            if (!attrs.id) {
+                attrs.$set('id', prefix + (++counter));
+            }
+ 
+            // CKEditor stuff
+            // Override the normal CKEditor save plugin
+ 
+            CKEDITOR.plugins.registered['save'] =
+            {
+                init: function (editor) {
+                    editor.addCommand('save',
+                        {
+                            modes: { wysiwyg: 1, source: 1 },
+                            exec: function (editor) {
+                                if (editor.checkDirty()) {
+                                    var ckValue = editor.getData();
+                                    scope.$apply(function () {
+                                        setter(scope, ckValue);
+                                    });
+                                    ckValue = null;
+                                    editor.resetDirty();
+                                }
+                            }
+                        }
+                    );
+                    editor.ui.addButton('Save', { label: 'Save', command: 'save', toolbar: 'document' });
+                }
+            };
+            var options = {};
+            options.on = {
+                blur: function (e) {
+                    if (e.editor.checkDirty()) {
+                        var ckValue = e.editor.getData();
+                        scope.$apply(function () {
+                            setter(scope, ckValue);
+                        });
+                        ckValue = null;
+                        e.editor.resetDirty();
+                    }
+                }
+            };
+            //options.extraPlugins = 'sourcedialog';
+            //options.removePlugins = 'sourcearea';
+            var editorangular = CKEDITOR.inline(element[0], options); //invoke
+ 
+            scope.$watch(attrs.ckedit, function (value) {
+                editorangular.setData(value);
+            });
+        }
+    }
+ 
+});
