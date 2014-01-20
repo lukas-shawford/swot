7c166ecdfa9606c72c90cae5f26ad606414be9a3
Sergey Krilov <serg.kr@gmail.com>
2014-01-19 23:31:38 -0500

    Edit Quiz: Confirmation popover for deleting questions
    Also color the delete button red on hover.

---
+++ client/app/common/confirmButton.js
@@ -0,0 +1,86 @@
+/*
+ * confirm-button directive
+ * ------------------------
+ *
+ * Add the confirm-button attribute to a button to make a confirmation popover appear before
+ * performing the specified action. Requires AngularUI and Bootstrap.
+ *
+ * Example usage:
+ * <button type="button" confirm-button="delete($index)" message="Are you sure?"></button>
+ *
+ * In the above example, delete($index) will *not* immediately be called when clicking the button.
+ * Instead, a confirmation popover will appear first with the message "Are you sure?", and the
+ * buttons "Yes" and "No". Only if the user clicks "Yes" will delete($index) actually be called.
+ *
+ * Additional options configurable via attributes:
+ *     - title="Confirm": Changes the title text on the popover
+ *     - yes="Yep": Changes the text of the "Yes" button
+ *     - no="Nope": Changes the text of the "No" button
+ *
+ * This was borrowed and slightly modified from here:
+ * http://wegnerdesign.com/blog/angular-js-directive-tutorial-on-attribute-bootstrap-confirm-button/
+ */
+angular.module('swot').directive('confirmButton', function ($document, $parse) {
+    return {
+        restrict: 'A',
+        link: function (scope, element, attrs) {
+            var buttonId, html, message, nope, title, yep;
+      
+            buttonId = Math.floor(Math.random() * 10000000000);
+
+            attrs.buttonId = buttonId;
+
+            message = attrs.message || "Are you sure?";
+            yep = attrs.yes || "Yes";
+            nope = attrs.no || "No";
+            title = attrs.title || "Confirm";
+      
+            html = "<div id=\"button-" + buttonId + "\">" +
+                        "<p class=\"confirmbutton-msg\">" + message + "</p>" +
+                        "<button class=\"confirmbutton-yes btn btn-danger\">" + yep + "</button>" +
+                        "<button class=\"confirmbutton-no btn\">" + nope + "</button>" +
+                    "</div>";
+
+            element.popover({
+                content: html,
+                html: true,
+                trigger: "manual",
+                title: "Confirm"
+            });
+
+            element.bind('click', function(e) {
+                var dontBubble = true;
+                e.stopPropagation();
+
+                element.popover('show');
+
+                var pop = $("#button-" + buttonId);
+
+                pop.closest(".popover").click(function(e) {
+                    if (dontBubble) {
+                        e.stopPropagation();
+                    }
+                });
+
+                pop.find('.confirmbutton-yes').click(function(e) {
+                    dontBubble = false;
+                    var func = $parse(attrs.confirmButton);
+                    scope.$apply(function () {
+                        func(scope);
+                    });
+                });
+
+                pop.find('.confirmbutton-no').click(function(e) {
+                    dontBubble = false;
+                    $document.off('click.confirmbutton.' + buttonId);
+                    element.popover('hide');
+                });
+
+                $document.on('click.confirmbutton.' + buttonId, ":not(.popover, .popover *)", function() {
+                    $document.off('click.confirmbutton.' + buttonId);
+                    element.popover('hide');
+                });
+            });
+        }
+    };
+});
