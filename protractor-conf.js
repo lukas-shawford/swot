ccfb394a73123fa6fe8c4f5b1b4d89849fdc841b
Sergey Krilov <serg.kr@gmail.com>
2014-01-29 21:46:41 -0500

    Start e2e tests
    Added Protractor as dependency, set up a configuration file, and add an onPrepare.js script that logs in as a test user. No actual tests yet.

---
+++ protractor-conf.js
@@ -0,0 +1,33 @@
+// Configuration file for end-to-end tests using Protractor.
+exports.config = {
+  
+  // The address of a running selenium server.
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+
+  // The base URL of the app we're testing
+  baseUrl: 'http://localhost:3000/',
+
+  // Capabilities to be passed to the webdriver instance.
+  capabilities: {
+    'browserName': 'chrome'
+  },
+
+  // Spec patterns are relative to the current working directly when
+  // protractor is called.
+  specs: ['test/e2e/**/*.js'],
+
+  // Options to be passed to Jasmine-node.
+  jasmineNodeOpts: {
+    showColors: true,
+    defaultTimeoutInterval: 30000
+  },
+
+  /**
+   * A callback function called once protractor is ready and available,
+   * and before the specs are executed.
+   *
+   * You can specify a file containing code to run by setting onPrepare to
+   * the filename string.
+   */
+  onPrepare: 'test/e2e/onPrepare.js'
+};
---
+++ test/e2e/onPrepare.js
@@ -0,0 +1,42 @@
+/**
+ * onPrepare.js
+ * ------------
+ *
+ * Preparation script for end-to-end tests using Protractor. This script gets run once, after
+ * Protractor has finished loading, but before any of the actual tests are run. The purpose of this
+ * script is to log in as a test user so the rest of the tests are run as an authenticated user.
+ *
+ * Since the login page does not use Angular, we have to use WebDriver directly rather than using
+ * Protractor (e.g., call browser.driver.get instead of browser.get). Additionally, we must inform
+ * Protractor when we are all ready to go using browser.driver.wait, which takes in a function
+ * that gets continuously invoked - when this function returns true, that signals Protractor that
+ * we are ready, at which point the actual tests will get executed.
+ *  
+ */
+
+// Use WebDriver directly, since the login page does not use Angular.
+var driver = browser.driver;
+
+// URL for the login page.
+var loginUrl = protractor.getInstance().baseUrl + '/login';
+
+// Test user credentials. The test user must be set up.
+var username = 'test@example.com';
+var password = 'tester';
+
+// Regex for the landing page URL after a successful login. After entering the credentials, we'll
+// tell WebDriver to keep testing the URL until it matches this pattern.
+var successfulLoginUrlRegex = /quizzes$/;
+
+// Log in
+driver.get(loginUrl);
+driver.findElement(by.name('email')).sendKeys(username);
+driver.findElement(by.name('password')).sendKeys(password);
+driver.findElement(by.xpath("//button[@type='submit']")).click();
+
+// Wait until we are logged in.
+driver.wait(function() {
+    return driver.getCurrentUrl().then(function(url) {
+        return /quizzes$/.test(url);
+    });
+});
