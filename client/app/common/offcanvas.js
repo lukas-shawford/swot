1a1f39ddfcd9f48d3ec870039fcaf90be0b7cae8
Sergey Krilov <serg.kr@gmail.com>
2014-03-05 22:12:20 -0500

    Slight fix for offcanvas sidebar on desktop
    Previously, there was an unnecessary scrollbar on the page on desktop. Now, it's still there, but only on mobile (still not sure how to make the sidebar not stack...)

--- client/app/common/offcanvas.js
+++ client/app/common/offcanvas.js
@@ -2,11 +2,4 @@ $(document).ready(function() {
     $('[data-toggle=offcanvas]').click(function() {
         $('.row-offcanvas').toggleClass('active');
     });
-
-    // This seems to fix an issue with the offcanvas sidebar taking up height and pushing the
-    // main content down (i.e., they're stacking, even though they're also side by side). I noticed
-    // that it was the height: auto; property on the sidebar that was causing this behavior. This
-    // sets the height to 0. It does have the side effect of breaking the sticky footer, but that
-    // won't be a problem for now since I'm planning on removing the sticky footer anyway.
-    $('.sidebar-offcanvas').height(0);
 });
