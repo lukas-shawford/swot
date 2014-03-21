0bba069eb93646ab465eff32e6de857f424ab936
Sergey Krilov <serg.kr@gmail.com>
2014-03-20 20:53:35 -0400

    Add validation for all question types

--- test/unit/question.js
+++ test/unit/question.js
@@ -1,3 +1,4 @@
+var mongoose = require('mongoose');
 var chai = require('chai');
 var sinon = require('sinon');
 var sinonChai = require('sinon-chai');
@@ -15,9 +16,47 @@ chai.use(sinonChai);
 var expect = chai.expect;
 var should = chai.should;
 
+var MONGODB_URL = process.env.MONGODB_TEST_URL || 'localhost:27017/swot_test';
+chai.Assertion.includeStack = true;
+
 describe('question', function () {
 
+    before(function (done) {
+        mongoose.connect(MONGODB_URL);
+        done();
+    });
+
+    after(function () {
+        mongoose.connection.close();
+    });
+
     describe('Fill In Questions', function () {
+        describe('validate', function () {
+
+            it('should validate successfully for a proper fill-in question', function () {
+                var question = new FillInQuestion({
+                    questionHtml: '<p>What is the capital of North Dakota?</p>',
+                    answer: 'Bismarck'
+                });
+
+                question.validate(function (err) {
+                    expect(err).to.not.exist;
+                });
+            });
+            
+            it('should return error if answer is missing', function () {
+                var question = new FillInQuestion({
+                    questionHtml: '<p>What is the capital of North Dakota?</p>'
+                });
+
+                question.validate(function (err) {
+                    expect(err).to.exist;
+                    expect(err.errors.answer).to.exist;
+                });
+            });
+            
+        });
+
         describe('submit', function () {
 
             it('should return true if the submission matches the correct answer', function () {
@@ -42,6 +81,48 @@ describe('question', function () {
     });
 
     describe('Multiple Choice Questions', function () {
+        describe('validate', function () {
+
+            it('should validate successfully for a proper multiple choice question', function () {
+                var question = new MultipleChoiceQuestion({
+                    questionHtml: '<p>What is the capital of North Dakota?</p>',
+                    choices: ['Pierre', 'Bismarck', 'Des Moines', 'Helena'],
+                    correctAnswerIndex: 1
+                });
+
+                question.validate(function (err) {
+                    expect(err).to.not.exist;
+                });
+            });
+            
+            it('should return error if correctChoiceIndex is larger than the number of choices', function () {
+                var question = new MultipleChoiceQuestion({
+                    questionHtml: '<p>What is the capital of North Dakota?</p>',
+                    choices: ['Pierre', 'Bismarck', 'Des Moines', 'Helena'],
+                    correctAnswerIndex: 4
+                });
+
+                question.validate(function (err) {
+                    expect(err).to.exist;
+                    expect(err.errors.correctAnswerIndex).to.exist;
+                });
+            });
+
+            it('should return error if there are no choices', function () {
+                var question = new MultipleChoiceQuestion({
+                    questionHtml: '<p>Uh oh, this question has no choices...</p>',
+                    choices: [],
+                    correctAnswerIndex: 0
+                });
+
+                question.validate(function (err) {
+                    expect(err).to.exist;
+                    expect(err.errors.choices).to.exist;
+                });
+            });
+
+        });
+
         describe('submit', function () {
 
             it('should return true if the submission matches the correct answer index', function () {
