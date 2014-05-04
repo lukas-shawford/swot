852812ac8c685759637ad1ccd8f2396a3325af8d
Sergey Krilov <serg.kr@gmail.com>
2014-05-04 00:07:09 -0400

    Tweak angular-xeditable to use btn-success instead of btn-primary for accept button

--- client/vendor/angular-xeditable/dist/js/xeditable.js
+++ client/vendor/angular-xeditable/dist/js/xeditable.js
@@ -1539,7 +1539,7 @@ angular.module('xeditable').factory('editableThemes', function() {
       inputTpl:    '',
       errorTpl:    '<div class="editable-error help-block" ng-show="$error" ng-bind="$error"></div>',
       buttonsTpl:  '<span class="editable-buttons"></span>',
-      submitTpl:   '<button type="submit" class="btn btn-primary"><span class="glyphicon glyphicon-ok"></span></button>',
+      submitTpl:   '<button type="submit" class="btn btn-success"><span class="glyphicon glyphicon-ok"></span></button>',
       cancelTpl:   '<button type="button" class="btn btn-default" ng-click="$form.$cancel()">'+
                      '<span class="glyphicon glyphicon-remove"></span>'+
                    '</button>',
