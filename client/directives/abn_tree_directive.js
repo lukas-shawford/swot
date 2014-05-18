b8df9aad78ac0f300bb9b24c1dad8cf63f4ee875
Sergey Krilov <serg.kr@gmail.com>
2014-05-17 21:57:57 -0400

    Make the abn-tree directive use a template URL instead of hardcoding the entirety of the template inline
    Also, make the template URL configurable via template-url attribute. Example: <abn-tree tree-data="..." template-url="/partials/abn_tree_template.html">

--- client/directives/abn_tree_directive.js
+++ client/directives/abn_tree_directive.js
@@ -7,7 +7,7 @@
     '$timeout', function($timeout) {
       return {
         restrict: 'E',
-        template: "<ul class=\"nav nav-list nav-pills nav-stacked abn-tree\">\n  <li ng-repeat=\"row in tree_rows | filter:{visible:true} track by row.branch.uid\" ng-animate=\"'abn-tree-animate'\" ng-class=\"'level-' + {{ row.level }} + (row.branch.selected ? ' active':'')\" class=\"abn-tree-row\">\n    <a ng-click=\"user_clicks_branch(row.branch)\">\n      <i ng-class=\"row.tree_icon\" ng-click=\"row.branch.expanded = !row.branch.expanded\" class=\"indented tree-icon\"> </i>\n      <span class=\"indented tree-label\">{{ row.label }} </span>\n    </a>\n  </li>\n</ul>",
+        templateUrl: function(element, attr) { return attr.templateUrl ? attr.templateUrl : '/partials/abn_tree_template.html' },
         replace: true,
         scope: {
           treeData: '=',
