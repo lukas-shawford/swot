6f046b3e84203f03ade55ee194d68f5fe553d95d
Sergey Krilov <serg.kr@gmail.com>
2014-05-31 23:32:46 -0400

    Remove unnecessary level in unit test hierarchy for quiz tests

--- test/unit/quiz.js
+++ test/unit/quiz.js
@@ -33,160 +33,159 @@ describe('quiz', function () {
         mongoose.connection.close();
     });
 
-    describe('quiz db (lib/quiz.js)', function () {
-        var testUserId;
-        var testQuizId;
+    var testUserId;
+    var testQuizId;
 
-        describe('createQuiz', function () {
 
-            it('should be able to create a quiz and associate it with a user.', function (done) {
-                User.createUser({
-                    email: 'test@example.com',
-                    password: 'tester'
-                }, function (err, user) {
-                    if (err) throw err;
-                    testUserId = user._id;
+    describe('createQuiz', function () {
 
-                    Quiz.createQuiz({
-                        name: 'My Test Quiz'
-                    }, user)
-                        .then(function (result) {
-                            var quiz = result[0];
-                            var user = result[1];
-                            expect(quiz).to.exist;
-                            expect(user._id.toString()).to.equal(testUserId.toString());
-                            testQuizId = quiz._id;
+        it('should be able to create a quiz and associate it with a user.', function (done) {
+            User.createUser({
+                email: 'test@example.com',
+                password: 'tester'
+            }, function (err, user) {
+                if (err) throw err;
+                testUserId = user._id;
 
-                            // Ensure quiz is associated with the user
-                            expect(quiz.createdBy.toString()).to.equal(testUserId.toString());  // Check quiz.createdBy
-                            User.findOne({ _id: testUserId }, function (err, user) {           // Check User.quizzes (need to reload document first because it's out of sync)
-                                if (err) throw err;
-                                expect(user.quizzes).to.contain(testQuizId);
-                                done();
-                            });
-                        })
-                        .catch(function (err) {
-                            throw err;
-                        }).done();
-                });
-            });
-
-            it('should save questions when creating a quiz', function (done) {
-                Q(User.findById(testUserId).exec())
-                    .then(function (user) {
-                        return Quiz.createQuiz({
-                            name: 'Night Flying',
-                            questions: [
-                                new FillInQuestion({
-                                    questionHtml: "What is the name of the photoreceptors in the retina of the eye that allow for color as well as detail vision?",
-                                    answer: "cones",
-                                    alternativeAnswers: ["cone"]
-                                }),
-                                new FillInQuestion({
-                                    questionHtml: "During a constant rate turn, you tilt your head down " +
-                                        "to change a fuel tank. The rapid head movement creates an overwhelming " +
-                                        "sensation of rotating, turning, or accelerating in a " +
-                                        "different direction. What is this illusion called?",
-                                    answer: "Coriolis Illusion",
-                                    ignoreCase: true,
-                                    alternativeAnswers: [
-                                        'coriolis',
-                                        'the coriolis illusion'
-                                    ]
-                                })
-                            ]
-                        }, user);
-                    })
+                Quiz.createQuiz({
+                    name: 'My Test Quiz'
+                }, user)
                     .then(function (result) {
                         var quiz = result[0];
-                        expect(quiz.questions).to.have.length(2);
-                        expect(quiz.questions[0]).to.be.an.instanceof(FillInQuestion);
-                        expect(quiz.questions[0].answer).to.equal('cones');
-                        expect(quiz.questions[1]).to.be.an.instanceof(FillInQuestion);
-                        expect(quiz.questions[1].answer).to.equal('Coriolis Illusion');
+                        var user = result[1];
+                        expect(quiz).to.exist;
+                        expect(user._id.toString()).to.equal(testUserId.toString());
+                        testQuizId = quiz._id;
+
+                        // Ensure quiz is associated with the user
+                        expect(quiz.createdBy.toString()).to.equal(testUserId.toString());  // Check quiz.createdBy
+                        User.findOne({ _id: testUserId }, function (err, user) {           // Check User.quizzes (need to reload document first because it's out of sync)
+                            if (err) throw err;
+                            expect(user.quizzes).to.contain(testQuizId);
+                            done();
+                        });
                     })
-                    .done(function () { done(); });
+                    .catch(function (err) {
+                        throw err;
+                    }).done();
             });
         });
 
-        describe('submitQuestion', function () {
+        it('should save questions when creating a quiz', function (done) {
+            Q(User.findById(testUserId).exec())
+                .then(function (user) {
+                    return Quiz.createQuiz({
+                        name: 'Night Flying',
+                        questions: [
+                            new FillInQuestion({
+                                questionHtml: "What is the name of the photoreceptors in the retina of the eye that allow for color as well as detail vision?",
+                                answer: "cones",
+                                alternativeAnswers: ["cone"]
+                            }),
+                            new FillInQuestion({
+                                questionHtml: "During a constant rate turn, you tilt your head down " +
+                                    "to change a fuel tank. The rapid head movement creates an overwhelming " +
+                                    "sensation of rotating, turning, or accelerating in a " +
+                                    "different direction. What is this illusion called?",
+                                answer: "Coriolis Illusion",
+                                ignoreCase: true,
+                                alternativeAnswers: [
+                                    'coriolis',
+                                    'the coriolis illusion'
+                                ]
+                            })
+                        ]
+                    }, user);
+                })
+                .then(function (result) {
+                    var quiz = result[0];
+                    expect(quiz.questions).to.have.length(2);
+                    expect(quiz.questions[0]).to.be.an.instanceof(FillInQuestion);
+                    expect(quiz.questions[0].answer).to.equal('cones');
+                    expect(quiz.questions[1]).to.be.an.instanceof(FillInQuestion);
+                    expect(quiz.questions[1].answer).to.equal('Coriolis Illusion');
+                })
+                .done(function () { done(); });
+        });
+    });
 
-            var testQuiz;
+    describe('submitQuestion', function () {
 
-            before(function (done) {
-                // Add some questions to the test quiz
-                Quiz.findOne({ _id: testQuizId }, function (err, quiz) {
+        var testQuiz;
+
+        before(function (done) {
+            // Add some questions to the test quiz
+            Quiz.findOne({ _id: testQuizId }, function (err, quiz) {
+                if (err) throw err;
+
+                quiz.questions.push(new FillInQuestion({
+                    questionHtml: "What is the capital of North Dakota?",
+                    answer: "Bismarck"
+                }));
+                quiz.questions.push(new FillInQuestion({
+                    questionHtml: "What is the default squawk code for VFR aircraft in the United States?",
+                    answer: "1200"
+                }));
+                quiz.questions.push(new FillInQuestion({
+                    questionHtml: "You are a senior executive at a Pharmacy Benefit Management (PBM) firm. After a recent acquisition of another PBM, your firm is now able to offer clients a wider range of sophisticated administrative and clinically-based services. Does this represent a strength or an opportunity according to SWOT analysis?",
+                    answer: "strength"
+                }));
+                quiz.save(function (err) {
                     if (err) throw err;
 
-                    quiz.questions.push(new FillInQuestion({
-                        questionHtml: "What is the capital of North Dakota?",
-                        answer: "Bismarck"
-                    }));
-                    quiz.questions.push(new FillInQuestion({
-                        questionHtml: "What is the default squawk code for VFR aircraft in the United States?",
-                        answer: "1200"
-                    }));
-                    quiz.questions.push(new FillInQuestion({
-                        questionHtml: "You are a senior executive at a Pharmacy Benefit Management (PBM) firm. After a recent acquisition of another PBM, your firm is now able to offer clients a wider range of sophisticated administrative and clinically-based services. Does this represent a strength or an opportunity according to SWOT analysis?",
-                        answer: "strength"
-                    }));
-                    quiz.save(function (err) {
+                    // Save a reference to the test quiz
+                    Quiz.findOne({ _id: testQuizId }, function (err, quiz) {
                         if (err) throw err;
-
-                        // Save a reference to the test quiz
-                        Quiz.findOne({ _id: testQuizId }, function (err, quiz) {
-                            if (err) throw err;
-                            testQuiz = quiz;
-                            done();
-                        });
+                        testQuiz = quiz;
+                        done();
                     });
                 });
             });
+        });
 
-            it('should return true if the submission matches the correct answer', function () {
-                var result = testQuiz.submitQuestion(0, "Bismarck");
-                expect(result.success).to.be.true;
-                expect(result.isCorrect).to.be.true;
+        it('should return true if the submission matches the correct answer', function () {
+            var result = testQuiz.submitQuestion(0, "Bismarck");
+            expect(result.success).to.be.true;
+            expect(result.isCorrect).to.be.true;
 
-                result = testQuiz.submitQuestion(1, "1200");
-                expect(result.success).to.be.true;
-                expect(result.isCorrect).to.be.true;
+            result = testQuiz.submitQuestion(1, "1200");
+            expect(result.success).to.be.true;
+            expect(result.isCorrect).to.be.true;
 
-                result = testQuiz.submitQuestion(2, "strength");
-                expect(result.success).to.be.true;
-                expect(result.isCorrect).to.be.true;
+            result = testQuiz.submitQuestion(2, "strength");
+            expect(result.success).to.be.true;
+            expect(result.isCorrect).to.be.true;
+        });
+
+        it('should return false if the submission does not match', function () {
+            var result = testQuiz.submitQuestion(0, "Pierre");
+            expect(result.success).to.be.true;
+            expect(result.isCorrect).to.be.false;
+            expect(result.correctAnswer).to.equal("Bismarck");
+
+            result = testQuiz.submitQuestion(1, "7700");
+            expect(result.success).to.be.true;
+            expect(result.isCorrect).to.be.false;
+            expect(result.correctAnswer).to.equal("1200");
+
+            result = testQuiz.submitQuestion(2, "opportunity");
+            expect(result.success).to.be.true;
+            expect(result.isCorrect).to.be.false;
+            expect(result.correctAnswer).to.equal("strength");
+        });
+
+        it('should return an error if the question index is out of range or invalid', function () {
+            expect(testQuiz.submitQuestion(-1, "Pierre")).to.deep.equal({
+                success: false,
+                message: "Invalid question index."
             });
-
-            it('should return false if the submission does not match', function () {
-                var result = testQuiz.submitQuestion(0, "Pierre");
-                expect(result.success).to.be.true;
-                expect(result.isCorrect).to.be.false;
-                expect(result.correctAnswer).to.equal("Bismarck");
-
-                result = testQuiz.submitQuestion(1, "7700");
-                expect(result.success).to.be.true;
-                expect(result.isCorrect).to.be.false;
-                expect(result.correctAnswer).to.equal("1200");
-
-                result = testQuiz.submitQuestion(2, "opportunity");
-                expect(result.success).to.be.true;
-                expect(result.isCorrect).to.be.false;
-                expect(result.correctAnswer).to.equal("strength");
+            expect(testQuiz.submitQuestion(3, "Pierre")).to.deep.equal({
+                success: false,
+                message: "Invalid question index."
             });
-
-            it('should return an error if the question index is out of range or invalid', function () {
-                expect(testQuiz.submitQuestion(-1, "Pierre")).to.deep.equal({
-                    success: false,
-                    message: "Invalid question index."
-                });
-                expect(testQuiz.submitQuestion(3, "Pierre")).to.deep.equal({
-                    success: false,
-                    message: "Invalid question index."
-                });
-                expect(testQuiz.submitQuestion("asparagus", "Pierre")).to.deep.equal({
-                    success: false,
-                    message: "Invalid question index."
-                });
+            expect(testQuiz.submitQuestion("asparagus", "Pierre")).to.deep.equal({
+                success: false,
+                message: "Invalid question index."
             });
         });
     });
